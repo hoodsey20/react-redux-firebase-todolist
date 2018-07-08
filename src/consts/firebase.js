4a6b73499e319323382dfc65ea8b68dc0223775b
Артём <hoodsey20@gmail.com>
2018-07-09 01:33:58 +0700

    конфиг firebase

---
+++ src/consts/firebase.js
@@ -0,0 +1,8 @@
+export const FB_CONFIG = {
+  apiKey: 'AIzaSyAE6j-05-qOPW99WALSuXSW7KWS8IIdX4U',
+  authDomain: 'todolist-142d0.firebaseapp.com',
+  databaseURL: 'https://todolist-142d0.firebaseio.com',
+  projectId: 'todolist-142d0',
+  storageBucket: 'todolist-142d0.appspot.com',
+  messagingSenderId: '1096132942049'
+};
