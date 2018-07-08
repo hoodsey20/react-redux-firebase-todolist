28915c86c6209dd1c539168b9c1989b1ae49c5b5
Артём <hoodsey20@gmail.com>
2018-07-09 01:36:49 +0700

    функция получения текстового предсталвения timestamp

---
+++ src/util.js
@@ -0,0 +1,12 @@
+export const getConvertedDate = (timestamp) => {
+  const a = new Date(timestamp);
+  const months = ['Января', 'Февраля', 'Марта', 'Апреля', 'Мая', 'Июня', 'Июля', 'Августа', 'Сентября', 'Октября', 'Ноября', 'Декабря'];
+  const year = a.getFullYear();
+  const month = months[a.getMonth()];
+  const date = a.getDate();
+  const hour = a.getHours();
+  const min = a.getMinutes();
+  const sec = a.getSeconds();
+  const time = `${date} ${month} ${year} ${hour}:${min}:${sec}`;
+  return time;
+};
